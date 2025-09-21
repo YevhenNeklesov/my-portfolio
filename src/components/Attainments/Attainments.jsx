c8050ddfa371b622b4b6840a12cfb6dad15d580c
YevhenNeklesov <161756863+YevhenNeklesov@users.noreply.github.com>
2025-09-21 21:59:50 +0200

    update homepage

--- src/components/Attainments/Attainments.jsx
+++ src/components/Attainments/Attainments.jsx
@@ -1,48 +1,132 @@
 import React from "react";
 import s from "./Attainments.module.css";
 import jefferson from "../../assets/images/jefferson-mp4.mp4";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css/sea-green";
 
 const Attainments = () => {
   return (
     <div className={s.container}>
-      <ul className={s.list}>
-        <li className={s.item}>
-          <a className={s.link} href="">
-            <p className={s.img}>Project name</p>
-          </a>
-        </li>
-        <li className={s.item}>
-          <a
-            className={s.link}
-            href="https://yevhenneklesov.github.io/red-hot-chili-coders/"
-            target="_blank"
-          >
-            <video autoPlay loop muted playsInline className={s.video}>
-              <source src={jefferson} type="video/mp4" />
-            </video>
-          </a>
-        </li>
-        <li className={s.item}>
-          <a className={s.link} href="">
-            <p className={s.img}>Project name</p>
-          </a>
-        </li>
-        <li className={s.item}>
-          <a className={s.link} href="">
-            <p className={s.img}>Project name</p>
-          </a>
-        </li>
-        {/* <li className={s.item}>
-          <a className={s.link} href="">
-            <p className={s.img}>Project name</p>
-          </a>
-        </li>
-        <li className={s.item}>
-          <a className={s.link} href="">
-            <p className={s.img}>Project name</p>
-          </a>
-        </li> */}
-      </ul>
+      <Splide
+        hasTrack={false}
+        aria-label="..."
+        options={{
+          rewind: true,
+          perMove: 1,
+          perPage: 2,
+          width: "1000px",
+          gap: "20px",
+          rewindByDrag: true,
+          padding: "5px",
+          arrows: false,
+          wheel: true,
+          reducedMotion: true,
+          autoplay: true,
+          interval: 4000,
+          pauseOnHover: true,
+        }}
+      >
+        <div className="custom-wrapper">
+          <SplideTrack>
+            <SplideSlide>
+              {" "}
+              <div className={s.slide}>
+                <a
+                  className={s.link}
+                  href="https://yevhenneklesov.github.io/red-hot-chili-coders/"
+                  target="_blank"
+                >
+                  {" "}
+                  <video autoPlay loop muted playsInline className={s.video}>
+                    {" "}
+                    <source src={jefferson} type="video/mp4" />{" "}
+                  </video>{" "}
+                </a>
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              {" "}
+              <div className={s.slide}>
+                <a
+                  className={s.link}
+                  href="https://yevhenneklesov.github.io/red-hot-chili-coders/"
+                  target="_blank"
+                >
+                  {" "}
+                  <video autoPlay loop muted playsInline className={s.video}>
+                    {" "}
+                    <source src={jefferson} type="video/mp4" />{" "}
+                  </video>{" "}
+                </a>
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              {" "}
+              <div className={s.slide}>
+                <a
+                  className={s.link}
+                  href="https://yevhenneklesov.github.io/red-hot-chili-coders/"
+                  target="_blank"
+                >
+                  {" "}
+                  <video autoPlay loop muted playsInline className={s.video}>
+                    {" "}
+                    <source src={jefferson} type="video/mp4" />{" "}
+                  </video>{" "}
+                </a>
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              {" "}
+              <div className={s.slide}>
+                <a
+                  className={s.link}
+                  href="https://yevhenneklesov.github.io/red-hot-chili-coders/"
+                  target="_blank"
+                >
+                  {" "}
+                  <video autoPlay loop muted playsInline className={s.video}>
+                    {" "}
+                    <source src={jefferson} type="video/mp4" />{" "}
+                  </video>{" "}
+                </a>
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              {" "}
+              <div className={s.slide}>
+                <a
+                  className={s.link}
+                  href="https://yevhenneklesov.github.io/red-hot-chili-coders/"
+                  target="_blank"
+                >
+                  {" "}
+                  <video autoPlay loop muted playsInline className={s.video}>
+                    {" "}
+                    <source src={jefferson} type="video/mp4" />{" "}
+                  </video>{" "}
+                </a>
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              {" "}
+              <div className={s.slide}>
+                <a
+                  className={s.link}
+                  href="https://yevhenneklesov.github.io/red-hot-chili-coders/"
+                  target="_blank"
+                >
+                  {" "}
+                  <video autoPlay loop muted playsInline className={s.video}>
+                    {" "}
+                    <source src={jefferson} type="video/mp4" />{" "}
+                  </video>{" "}
+                </a>
+              </div>
+            </SplideSlide>
+          </SplideTrack>
+        </div>
+      </Splide>
     </div>
   );
 };
